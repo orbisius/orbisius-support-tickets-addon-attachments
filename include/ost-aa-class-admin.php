bdd07a3275051f2c43fb67b3f3fdb534161e626d
Slavi Marinov <admin@orbisius.com>
2019-07-19 16:14:39 +0300

    removed the check on admin_init for ORBISIUS_SUPPORT_TICKETS_ATTACHMENTS_ADDON_FILES_DIR existence

--- include/ost-aa-class-admin.php
+++ include/ost-aa-class-admin.php
@@ -1,27 +1,12 @@
 <?php
 
-class Orbisius_Support_Tickets_Attachments_Addon_Admin {
-
-    function __construct() {
-        add_action('admin_init', array($this, 'init'));
-    }
-
-    public function init() {
-        $this->create_attachments_files_directory();
-    }
-
-    public function create_attachments_files_directory() {
-        if (!is_dir(ORBISIUS_SUPPORT_TICKETS_ATTACHMENTS_ADDON_FILES_DIR)) {
-            try {
-                if (!wp_mkdir_p(ORBISIUS_SUPPORT_TICKETS_ATTACHMENTS_ADDON_FILES_DIR)) {
-                    throw new Exception("Error creating the support tickets main folder");
-                }
-            } catch (Exception $ex) {
-                wp_die();
-            }
-        }
-    }
-
-}
-
 new Orbisius_Support_Tickets_Attachments_Addon_Admin();
+
+class Orbisius_Support_Tickets_Attachments_Addon_Admin {
+    function __construct() {
+        add_action('admin_init', array($this, 'admin_init'));
+    }
+
+    public function admin_init() {
+    }
+}
