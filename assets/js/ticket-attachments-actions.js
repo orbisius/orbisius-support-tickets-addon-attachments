2623452193f1ca70452ffa947b1849df5d8eb8a0
Carlos Rafael Garcia Caballero <charles890123@gmail.com>
2019-07-22 09:12:35 -0400

    Add feature to allow user continue submit attachments

--- assets/js/ticket-attachments-actions.js
+++ assets/js/ticket-attachments-actions.js
@@ -1,4 +1,7 @@
 jQuery(document).ready(function () {
+    /*
+     * Delete link
+     */
     jQuery('.ticket_attachment_delete').click(function (ev) {
         ev.preventDefault();
         var link = jQuery(this);
@@ -20,9 +23,34 @@ jQuery(document).ready(function () {
             if (data === "OK") {
                 link.parent('li').fadeOut('fast').remove();
             } else {
-                alert(data);                
+                alert(data);
                 link.attr('disabled', false);
             }
+        }).fail(function () {
+
+        });
+    });
+    /*
+     * New attachement form
+     */
+    jQuery('#orbisius_support_tickets_attachments_form').submit(function (ev) {
+        ev.preventDefault();
+        var form = jQuery(this);
+        var data = new FormData(form[0]);
+        data.append("action", "orbisius_support_tickets_action_new_file");
+        data.append("ticket_id", form.attr('data-id'));
+        jQuery.ajax({
+            method: "POST",
+            url: OST_AA.ajaxurl,
+            data: data,
+            cache: false,
+            processData: false,
+            contentType: false,
+            beforeSend: function () {
+
+            }
+        }).done(function (data) {
+            location.reload();
         }).fail(function () {
 
         });
